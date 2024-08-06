5a5ccaf5194b93743d684d2e4a039740d029ddf7
translator4geo <100690600+translator4geo@users.noreply.github.com>
2024-08-06 09:14:36 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,45 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//   let newName = document.querySelectorAll('#input').value;
-//   console.log(newName);
-//   document.addEventListener('click', function () {
-//     document.querySelector('.answer').innerHTML = `ახლა მე ${newName} ვარ`;
-//   });
-// });
-
 document.querySelector('#submit').addEventListener('click', function () {
   let newName = document.querySelector('#input').value;
   document.querySelector('.answer').innerHTML = `ახლა მე ${newName} ვარ`;
-  if (newName === 'ეკა') {
-    document.querySelector('.answer').innerHTML = `რა გვარი ხარ  ${newName}?`;
-  } else if (newName === 'ჩხოლარია') {
-    document.querySelector(
-      '.answer'
-    ).innerHTML = `აა, ვიცი, ბაჩოს ცოლი ხარ, აი შენი ფოტოც!`;
-    document.querySelector('#image').style.display = 'block';
-  } else if (newName === 'ლუკა') {
-    document.querySelector(
-      '.answer'
-    ).innerHTML = `ააა, ლუკა რამიშვილი ხარ შენ! ბაჩოს შვილი ხარ. პატარა ფისო!`;
-    document.querySelector('#photo').style.display = 'block';
-  } else if (newName === 'ქეთი') {
-    document.querySelector(
-      '.answer'
-    ).innerHTML = `გიცანი ქეთი! აი შენი ფოტოც, აჰაჰა!`;
-    document.querySelector('#image').style.display = 'block';
-  } else if (newName === 'ირმა') {
-    document.querySelector(
-      '.answer'
-    ).innerHTML = `აჰა, ბედკრული საქართველოს შვილი, ირმა ვირიშვილი!`;
-    document.querySelector('#image').style.display = 'block';
+
+  switch (newName) {
+    case 'ეკა':
+      document.querySelector('.answer').innerHTML = `რა გვარი ხარ  ${newName}?`;
+      break;
+    case 'ლუკა':
+      document.querySelector(
+        '.answer'
+      ).innerHTML = `ააა, ლუკა რამიშვილი ხარ შენ! ბაჩოს შვილი ხარ. პატარა ფისო!`;
+      document.querySelector('#photo').style.display = 'block';
+      break;
+    case 'ქეთი':
+      document.querySelector(
+        '.answer'
+      ).innerHTML = `გიცანი ქეთი! აი შენი ფოტოც, აჰაჰა!`;
+      document.querySelector('#image').style.display = 'block';
+      break;
+    case 'ირმა':
+      document.querySelector(
+        '.answer'
+      ).innerHTML = `აჰა, ბედკრული საქართველოს შვილი, ირმა ვირიშვილი!`;
+      document.querySelector('#image').style.display = 'block';
+      break;
+    case 'ჩხოლარია':
+      document.querySelector(
+        '.answer'
+      ).innerHTML = `აა, ვიცი, ბაჩოს ცოლი ხარ, აი შენი ფოტოც!`;
+      document.querySelector('#image').style.display = 'block';
+      break;
+    case 'მარიამი':
+      document.querySelector(
+        '.answer'
+      ).innerHTML = `გამარჯობა შენი, ბოშ. რა გვარი ხარ მაგალითად?`;
+      break;
+    case 'ჭკადუა':
+      document.querySelector(
+        '.answer'
+      ).innerHTML = `ააა, ვიცი, მაკას გოგო ხარ. აი შენი საოცნებო მანქანა!`;
+      document.querySelector('#mariam').style.display = 'block';
+      break;
   }
 });
